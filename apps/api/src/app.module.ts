693fa82c5a57739a3c5e78367e216eca95d354d8
Lee-Ki-Bong <dhkehd2@gmail.com>
2025-01-12 19:55:45 +0900

    feat(api): configure PostgreSQL with TypeORM and update environment settings

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,13 +1,29 @@
 import { Module } from "@nestjs/common"
 import { AppController } from "./app.controller"
 import { AppService } from "./app.service"
-import { ConfigModule } from "@nestjs/config"
+import { ConfigModule, ConfigService } from "@nestjs/config"
+import { TypeOrmModule } from "@nestjs/typeorm"
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: [`.env.${process.env.NODE_ENV}`]
+      envFilePath: [".env.local"]
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        type: "postgres",
+        host: await configService.getOrThrow("DB_HOST"),
+        port: (await configService.getOrThrow("DB_PORT")) as number,
+        username: await configService.getOrThrow("DB_USERNAME"),
+        password: await configService.getOrThrow("DB_PASSWORD"),
+        database: await configService.getOrThrow("DB_DATABASE"),
+        autoLoadEntities: true,
+        synchronize: false,
+        logging: false,
+        charset: "utf8mb4"
+      }),
+      inject: [ConfigService]
     })
   ],
   controllers: [AppController],
