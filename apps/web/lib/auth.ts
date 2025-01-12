5aa7ad551503f2deda50ce2cb1ddb18b8b941d1d
Lee-Ki-Bong <dhkehd2@gmail.com>
2025-01-12 23:23:55 +0900

    feat(web): implement backend URL configuration and update signup logic

--- apps/web/lib/auth.ts
+++ apps/web/lib/auth.ts
@@ -1,5 +1,7 @@
 "use server"
 
+import { redirect } from "next/navigation"
+import { BACKEND_URL } from "./constants"
 import { FormState, SignupFormSchema } from "./type"
 
 export async function signUp(
@@ -7,19 +9,35 @@ export async function signUp(
   formData: FormData
 ): Promise<FormState> {
   // 1. Validate the form data
-  const validationFieldss = SignupFormSchema.safeParse({
+  const validationFields = SignupFormSchema.safeParse({
     name: formData.get("name") as string,
     email: formData.get("email") as string,
     password: formData.get("password") as string
   })
 
-  if (!validationFieldss.success) {
+  if (!validationFields.success) {
     return {
-      error: validationFieldss.error.flatten().fieldErrors
+      error: validationFields.error.flatten().fieldErrors
     }
   }
 
   // 2. Send the data to the server
+  const response = await fetch(`${BACKEND_URL}/auth/signup`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(validationFields.data)
+  })
 
-  return state
+  if (!response.ok) {
+    redirect("/auth/signin")
+  } else {
+    return {
+      message:
+        response.status === 409
+          ? "Account created successfully."
+          : response.statusText
+    }
+  }
 }
